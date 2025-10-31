b55ede55c1abcf3e6ad9c07398fd078e38408343
samarthangre <sammyangre2002@gmail.com>
2025-10-31 15:13:35 +0530

    baseurl

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const BASE_URL = "https://dumbchefs-backend.onrender.com" || "http://localhost:5000";
 
 export default BASE_URL;
